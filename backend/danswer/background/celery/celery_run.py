37e7cc62db2c98804533bd7578d8e17a625465d3
Kersten Behrens <kersten.behrens@label-software.de>
2024-09-06 22:29:58 +0200

    Fixed error "variable_functionality.py   41: Failed to fetch versioned implementation for danswer.background.celery.celery_app.celery_app: No module named 'backend' "

--- backend/danswer/background/celery/celery_run.py
+++ backend/danswer/background/celery/celery_run.py
@@ -5,5 +5,5 @@ from backend.danswer.utils.variable_functionality import set_is_ee_based_on_env_
 
 set_is_ee_based_on_env_variable()
 celery_app = fetch_versioned_implementation(
-    "danswer.background.celery.celery_app", "celery_app"
+    "backend.danswer.background.celery.celery_app", "celery_app"
 )
